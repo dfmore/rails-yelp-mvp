04f5406e6fa0b9d430619261866328e05023e86e
Daniel Moreira <de8a88@gmail.com>
2018-11-28 19:33:06 +0000

    Finished exercise

--- Gemfile
+++ Gemfile
@@ -32,11 +32,16 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+# This is from the Le Wagon
 gem 'bootstrap-sass'
 gem 'font-awesome-sass', '~> 5.0.9'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'jquery-rails'
+
+# Faker gem to generate fake restaurants
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = Array.new(5) do
+  {
+    name:         Faker::Movies::LordOfTheRings.location,
+    address:      Faker::Address.full_address,
+    category:     %w[chinese italian japanese french belgian].sample
+  }
+end
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
